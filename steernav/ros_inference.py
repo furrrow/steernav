d572f2c0750600c3852c5b8facc91a04cb1a8ebf
furrrow <jianyu234@gmail.com>
2026-09-13 16:31:01 -0400

    bounding box update

--- steernav/ros_inference.py
+++ steernav/ros_inference.py
@@ -373,12 +373,23 @@ class SteeringNode(Node):
                 pos_dict = {}
                 for box, id in zip(detections.xyxy, detections.tracker_id):
                     x1, y1, x2, y2 = box
+                    # update box size
+                    box_width = x2 - x1
+                    box_height = y2 - y1
+
+                    x1 += 0.10 * box_width
+                    x2 -= 0.10 * box_width
+                    y1 += 0.10 * box_height
+                    y2 -= 0.10 * box_height
+
                     box_3d_pts = points_input[int(y1):int(y2), int(x1):int(x2)]  # (120, 40, 3)
                     if box_3d_pts.size == 0:
                         continue
                     pts_flat = box_3d_pts.reshape(-1, 3)  # (N, 3)
                     valid_mask = ~np.isnan(pts_flat).any(axis=1) & (pts_flat != 0).any(axis=1)  # (N,)
                     valid_pts = pts_flat[valid_mask]  # (N, 3)
+                    if len(valid_pts) == 0:
+                        continue
                     median_3d = np.median(valid_pts, axis=0)
                     pos_dict[id] = median_3d
                     # print(f"detect id {id} median loc: {median_3d}")
@@ -403,7 +414,7 @@ class SteeringNode(Node):
             self.get_logger().info(f"robot_velocity_camera: {robot_velocity_camera}")
             # update points according to velocity obstacles...
             updated_points, point_movement_cam = update_points(points_input, self.detection_queue,
-                                                               robot_velocity_camera=robot_velocity_camera, last_n_records=2,
+                                                               robot_velocity_camera=robot_velocity_camera, last_n_records=3,
                                                                time_incr=0.5, time_look_ahead=1.0)
             point_movement_bev = [
                 (
